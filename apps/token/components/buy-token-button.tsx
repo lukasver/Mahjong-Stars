92d3f359b0bf973d64d6e47d6c40623488f6ce0d
Lucas Verdiell <l.verdiell@gmail.com>
2025-07-15 12:16:16 +0100

    refactor(buy-token-button, sidebar): update navigation links to reflect new routing structure; standardize import statements for consistency; improve code formatting for better readability.

--- apps/token/components/buy-token-button.tsx
+++ apps/token/components/buy-token-button.tsx
@@ -13,7 +13,7 @@ export async function BuyTokenButton({ className }: { className?: string }) {
     return null;
   }
   return (
-    <Link prefetch href={`/buy`}>
+    <Link prefetch href={`/dashboard/buy`}>
       <RainbowButton
         className={cn(
           'font-head border-2 shadow-sm border-solid bg-accent rounded-xl h-full hover:bg-accent/80 transition-all duration-300 hover:scale-105 hover:animate-pulse',
