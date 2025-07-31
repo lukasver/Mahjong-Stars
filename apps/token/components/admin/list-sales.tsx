8315b8078745966efa7921d041483d75068bc3f5
Lucas Verdiell <l.verdiell@gmail.com>
2025-07-31 11:42:46 +0100

    refactor(token): comment out unused DropdownMenuSeparator in ListSales component for cleaner code

--- apps/token/components/admin/list-sales.tsx
+++ apps/token/components/admin/list-sales.tsx
@@ -379,7 +379,7 @@ export function ListSales({
                                 </DropdownMenuSubContent>
                               </DropdownMenuPortal>
                             </DropdownMenuSub>
-                            <DropdownMenuSeparator />
+                            {/* <DropdownMenuSeparator /> */}
                             {/* <DropdownMenuItem className='text-destructive'>
                               <Trash2 className='mr-2 h-4 w-4' />
                               Delete Sale
