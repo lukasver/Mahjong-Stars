5a6e33a65230a96315941cbe76a2f9808efb5981
Lucas Verdiell <l.verdiell@gmail.com>
2025-08-07 19:47:50 +0100

    refactor(crons): replace existing webhook handler with a dedicated transactions cleanup route, maintaining API key validation and response structure

---
+++ apps/token/app/api/webhooks/crons/sessions/route.ts
@@ -0,0 +1,39 @@
+import { env } from '@/common/config/env';
+import { invariant } from '@epic-web/invariant';
+import logger from '@/lib/services/logger.server';
+import users from '@/lib/repositories/users';
+
+const handler = async (req: Request) => {
+  try {
+    const apiKey = req.headers.get('Authorization');
+    invariant(apiKey, 'API key is required');
+    const [type, token] = apiKey.split(' ');
+    invariant(type === 'Bearer', 'Invalid API key');
+    invariant(token === env.CRON_SERVICE_API_KEY, 'Invalid API key');
+
+    if (['POST', 'PUT', 'DELETE'].includes(req.method)) {
+      return Response.json(
+        { success: false, message: 'Not implemented' },
+        { status: 501 }
+      );
+    }
+
+    if (req.method === 'GET') {
+      await users.crons.cleanUp('all');
+      return Response.json({ success: true, message: 'OK' }, { status: 200 });
+    }
+
+    return Response.json(
+      { success: false, message: 'Bad request' },
+      { status: 400 }
+    );
+  } catch (e: unknown) {
+    logger(e);
+    return Response.json(
+      { success: false, message: 'Unauthorized' },
+      { status: 401 }
+    );
+  }
+};
+
+export const GET = handler;
