b8a910411b99e97e62c1b309cbc47939caa4a425
Lucas Verdiell <l.verdiell@gmail.com>
2025-10-23 10:56:24 +0100

    style/open in new external links

--- apps/token/components/nav/nav-main.tsx
+++ apps/token/components/nav/nav-main.tsx
@@ -1,12 +1,11 @@
-'use client';
+"use client";
 
-import { ChevronRight, type LucideIcon } from 'lucide-react';
-
+import { cn } from "@mjs/ui/lib/utils";
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
-} from '@mjs/ui/primitives/collapsible';
+} from "@mjs/ui/primitives/collapsible";
 import {
   SidebarGroup,
   SidebarGroupLabel,
@@ -16,8 +15,9 @@ import {
   SidebarMenuSub,
   SidebarMenuSubButton,
   SidebarMenuSubItem,
-} from '@mjs/ui/primitives/sidebar';
-import AppLink from '../link';
+} from "@mjs/ui/primitives/sidebar";
+import { ChevronRight, ExternalLink, type LucideIcon } from "lucide-react";
+import AppLink from "../link";
 
 export function NavMain({
   items,
@@ -30,6 +30,7 @@ export function NavMain({
     items?: {
       title: string;
       url: string;
+      openInNew?: boolean;
     }[];
   }[];
 }) {
@@ -42,14 +43,14 @@ export function NavMain({
             key={item.title}
             asChild
             defaultOpen={item.isActive}
-            className='group/collapsible'
+            className="group/collapsible"
           >
             <SidebarMenuItem>
               <CollapsibleTrigger asChild>
                 <SidebarMenuButton tooltip={item.title}>
                   {item.icon && <item.icon />}
                   <span>{item.title}</span>
-                  <ChevronRight className='ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90' />
+                  <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
                 </SidebarMenuButton>
               </CollapsibleTrigger>
               <CollapsibleContent>
@@ -57,8 +58,23 @@ export function NavMain({
                   {item.items?.map((subItem) => (
                     <SidebarMenuSubItem key={subItem.title}>
                       <SidebarMenuSubButton asChild>
-                        <AppLink href={subItem.url} prefetch={true}>
-                          <span>{subItem.title}</span>
+                        <AppLink
+                          href={subItem.url}
+                          prefetch={true}
+                          target={subItem.openInNew ? "_blank" : undefined}
+                        >
+                          <div
+                            className={cn(
+                              subItem.openInNew && "flex items-center gap-2",
+                            )}
+                          >
+                            <span className={cn(subItem.openInNew && "flex-1")}>
+                              {subItem.title}
+                            </span>
+                            {subItem.openInNew && (
+                              <ExternalLink className="size-2 shrink-0" />
+                            )}
+                          </div>
                         </AppLink>
                       </SidebarMenuSubButton>
                     </SidebarMenuSubItem>
--- apps/token/components/sidebar.tsx
+++ apps/token/components/sidebar.tsx
@@ -1,76 +1,78 @@
-'use client';
+"use client";
 
-import { Globe, LayoutDashboard } from 'lucide-react';
-import * as React from 'react';
-
+import ErrorBoundary from "@mjs/ui/components/error-boundary";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
   SidebarRail,
-} from '@mjs/ui/primitives/sidebar';
-import { metadata } from '../common/config/site';
-import { Logo } from './logo';
-import { NavMain } from './nav/nav-main';
-import { NavUser } from './nav/nav-user';
-import { TeamSwitcher } from './nav/team-switcher';
-import ErrorBoundary from '@mjs/ui/components/error-boundary';
+} from "@mjs/ui/primitives/sidebar";
+import { Globe, LayoutDashboard } from "lucide-react";
+import * as React from "react";
+import { metadata } from "../common/config/site";
+import { Logo } from "./logo";
+import { NavMain } from "./nav/nav-main";
+import { NavUser } from "./nav/nav-user";
+import { TeamSwitcher } from "./nav/team-switcher";
 
 // This is sample data.
 const data = {
   user: {
-    name: 'Anonymous',
-    email: 'anonymous@example.com',
-    avatar: '/static/images/avatars/anonymous.png',
+    name: "Anonymous",
+    email: "anonymous@example.com",
+    avatar: "/static/images/avatars/anonymous.png",
   },
   teams: [
     {
       name: metadata.businessName,
       logo: (props: React.SVGProps<SVGSVGElement>) => (
-        <Logo variant='icon' {...props} />
+        <Logo variant="icon" {...props} />
       ),
-      plan: '',
+      plan: "",
     },
   ],
   navMain: [
     {
-      title: 'Overview',
-      url: '#',
+      title: "Overview",
+      url: "#",
       icon: LayoutDashboard,
       isActive: true,
       items: [
         {
-          title: 'Dashboard',
-          url: '/dashboard',
+          title: "Dashboard",
+          url: "/dashboard",
           isActive: true,
         },
         {
-          title: 'Buy',
-          url: '/dashboard/buy',
+          title: "Buy",
+          url: "/dashboard/buy",
         },
         {
-          title: 'Transactions',
-          url: '/dashboard/transactions',
+          title: "Transactions",
+          url: "/dashboard/transactions",
         },
       ],
     },
     {
-      title: 'More info',
-      url: '#',
+      title: "More info",
+      url: "#",
       icon: Globe,
       items: [
         {
-          title: 'Tokenomics',
+          title: "Tokenomics",
           url: `/docs/${process.env.NEXT_PUBLIC_TOKENOMICS_SLUG}`,
+          openInNew: true,
         },
         {
-          title: 'Schedule',
+          title: "Schedule",
           url: `/docs/${process.env.NEXT_PUBLIC_SCHEDULE_SLUG}`,
+          openInNew: true,
         },
         {
-          title: 'Whitepaper',
-          url: process.env.NEXT_PUBLIC_WHITEPAPER_SLUG || '',
+          title: "Whitepaper",
+          url: process.env.NEXT_PUBLIC_WHITEPAPER_SLUG || "",
+          openInNew: true,
         },
       ],
     },
@@ -84,7 +86,7 @@ export function DashboardSidebar({
   children?: React.ReactNode;
 }) {
   return (
-    <Sidebar collapsible='icon' {...props}>
+    <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
         <ErrorBoundary fallback={null}>
           <TeamSwitcher teams={data.teams} />
